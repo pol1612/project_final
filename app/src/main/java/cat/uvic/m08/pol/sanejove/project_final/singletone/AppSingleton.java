9319653f6c939f30c8e89f156efa10eb40a6110e
pol sañé jové <pol.sane@uvic.cat>
2022-11-23 23:43:33 +0100

    makes a select and returns it in json

--- app/src/main/java/cat/uvic/m08/pol/sanejove/project_final/singletone/AppSingleton.java
+++ app/src/main/java/cat/uvic/m08/pol/sanejove/project_final/singletone/AppSingleton.java
@@ -2,17 +2,23 @@ package cat.uvic.m08.pol.sanejove.project_final.singletone;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import cat.uvic.m08.pol.sanejove.project_final.activities.async.FetchPizzaOrdersData;
 import cat.uvic.m08.pol.sanejove.project_final.adapter.PizzaOrderAdapter;
 import cat.uvic.m08.pol.sanejove.project_final.entities.PizzaOrder;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import com.google.gson.Gson;
@@ -29,6 +35,7 @@ public class AppSingleton extends AppCompatActivity {
     private PizzaOrder pizzaOrder;
     private PizzaOrderAdapter pizzaOrderAdapter;
     private Context appContext;
+    private String jsonData;
 
     private static class SingletonInstance {
         private static AppSingleton INSTANCE = new AppSingleton();
@@ -44,10 +51,12 @@ public class AppSingleton extends AppCompatActivity {
     }
 
     public  void setPizzOrderArrayToMySQLDataBase(){
-            AsyncTask fetchPizzaOrdersData=new FetchPizzaOrdersData().execute();
-            String jsonData="";
-            Type userListType = new TypeToken<ArrayList<PizzaOrder>>(){}.getType();
-            pizzaOrderArrayList = new Gson().fromJson(jsonData, userListType);
+        new FetchPizzaOrdersData().execute();
+        System.out.println("jsonData from table in database in server: \n"+jsonData);
+
+        jsonData="[]";
+        Type userListType = new TypeToken<ArrayList<PizzaOrder>>(){}.getType();
+        pizzaOrderArrayList = new Gson().fromJson(jsonData, userListType);
 
     }
     public void updatePizzaOrderArrayListItem(){
@@ -134,4 +143,71 @@ public class AppSingleton extends AppCompatActivity {
     public void setAppContext(Context appContext) {
         this.appContext = appContext;
     }
+
+    private class FetchPizzaOrdersData extends AsyncTask<Void,Void,String> {
+        private String sql;
+        public FetchPizzaOrdersData(){
+            super();
+        }
+        @Override
+        protected String doInBackground(Void... voids) {
+            System.out.println("started doInBackground");
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            String pizzaOrdersJsonStr = null;
+            String sql="SELECT * FROM Pizza_Order;";
+            try{
+
+                URL url=new URL("https://puntjif.com/teknos/gateway.php?sql="+sql);
+                urlConnection=(HttpURLConnection) url.openConnection();
+                if(urlConnection==null){
+                    System.out.println("url connection null");
+                }
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                pizzaOrdersJsonStr = buffer.toString();
+                System.out.println("pizzaOrdersJsonStr: "+pizzaOrdersJsonStr);
+                return pizzaOrdersJsonStr;
+            }catch (IOException e){
+                Log.e("PlaceholderFragment", "Error ", e);
+                return null;
+            }finally {
+                if(urlConnection!=null){
+                    urlConnection.disconnect();
+                }if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+                }
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            jsonData=s;
+            System.out.println("setted jsonData");
+        }
+    }
+
 }
